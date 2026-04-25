b28338efce8158de800a2b6c5e5f14a5dd145644
Dilna <dilna@example.com>
2026-04-25 10:48:47 +0100

    even or odd

--- Dilna/5-Even_or_Odd.py
+++ Dilna/5-Even_or_Odd.py
@@ -1,1 +1,11 @@
-# Write a function even_or_odd(number) that prints "Even" if the number is even, and "Odd" if it is odd. Test with 4, 7, 0, -3
+# Write a function even_or_odd(number) that prints "Even" if the number is even,
+# and "Odd" if it is odd. Test with 4, 7, 0, -3
+def even_or_odd(number):
+    if number % 2 == 0:
+        print("even ")
+    else :
+        print("odd")
+even_or_odd(4)
+even_or_odd(7)
+even_or_odd(0)
+even_or_odd(-3)
--- Dilna/6-Swap_Two_Variables.py
+++ Dilna/6-Swap_Two_Variables.py
@@ -1,1 +1,3 @@
-# Write a function swap(a, b) that prints the values of a and b before swapping, then swaps them (using a temporary variable), and prints the values after swapping. Call it with (10, 20) and ("cat", "dog")
+# Write a function swap(a, b) that prints the values of a and b before swapping,
+# then swaps them (using a temporary variable), and prints the values after swapping.
+# Call it with (10, 20) and ("cat", "dog")
