3666183b68f1b6a6d50a3910a93498847fe43ef8
Dilna <dilna@example.com>
2026-04-25 09:48:26 +0100

    added 2 numbers

--- Dilna/Add_Two_Numbers.py
+++ Dilna/Add_Two_Numbers.py
@@ -1,1 +1,7 @@
-## Write a function add(a, b) that prints the sum of a and b. Call it with numbers like (3, 5), (10, -2), (0, 0).
+# Write a function add(a, b) that prints the sum of a and b.
+# Call it with numbers like (3, 5), (10, -2), (0, 0).
+def function(a,b):
+    print("sum is",a+b)
+function(3,4)
+function(10,-2)
+function(0,0)
