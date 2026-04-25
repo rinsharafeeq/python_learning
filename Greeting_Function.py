f49a004b8e94cb8636923f3a95d729b42453cbf8
Rinsha Rafeeq <rinsharafeeq55@gmail.com>
2026-04-25 13:43:09 +0530

    function code

---
+++ Greeting_Function.py
@@ -0,0 +1,8 @@
+#Write a function greet(name) that takes a name as a parameter and prints "Hello, <name>!".
+# Then call it with three different names.
+
+def greet(name):
+    print(f"hello {name}")
+greet("rinsha")
+greet("rishana")
+greet("dilna")
