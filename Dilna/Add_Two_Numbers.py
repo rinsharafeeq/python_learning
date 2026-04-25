6df1b1cdda862e9e504d7481cada354b81859630
Dilna <dilna@example.com>
2026-04-25 10:04:15 +0100

    rectangle area

--- Dilna/Add_Two_Numbers.py
+++ Dilna/Add_Two_Numbers.py
@@ -1,5 +1,8 @@
 # Write a function add(a, b) that prints the sum of a and b.
 # Call it with numbers like (3, 5), (10, -2), (0, 0).
+from xml.dom import pulldom
+
+
 def function(a,b):
     print("sum is",a+b)
 function(3,4)
--- Dilna/Rectangle_Area.py
+++ Dilna/Rectangle_Area.py
@@ -1,1 +1,6 @@
-#Write a function rectangle_area(length, width) that calculates and prints the area. Then call it with (5, 3) and (2.5, 4).
+#Write a function rectangle_area(length, width) that calculates and prints the area.
+# Then call it with (5, 3) and (2.5, 4).
+def rectangle_area(length,width):
+    print("area = ",length*width)
+rectangle_area(5,3)
+rectangle_area(2.5,4)
