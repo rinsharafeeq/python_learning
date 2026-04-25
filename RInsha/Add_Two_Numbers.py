efbe68d946c347966475cdb617fded45edfea839
Rinsha Rafeeq <rinsharafeeq55@gmail.com>
2026-04-25 14:16:24 +0530

    sum of two numbers

--- RInsha/Add_Two_Numbers.py
+++ RInsha/Add_Two_Numbers.py
@@ -1,1 +1,6 @@
 ## Write a function add(a, b) that prints the sum of a and b. Call it with numbers like (3, 5), (10, -2), (0, 0).
+def add(a,b):
+    print(f"sum of {a} and {b} = {a+b}")
+add(3,5)
+add(10, -2)
+add(0,0)
