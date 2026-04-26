0294486584281931f6e08db8a6fb5de4bc2941ec
Dilna <dilna@example.com>
2026-04-26 09:09:50 +0100

    fibonacci sequence

--- Dilna/Next_Programs/9-_Fibonacci_Sequence_first_N_numbers.py
+++ Dilna/Next_Programs/9-_Fibonacci_Sequence_first_N_numbers.py
@@ -1,5 +1,12 @@
-# Write a function fibonacci(n) that prints the first n numbers of the Fibonacci sequence (starting with 0, 1).
+# Write a function fibonacci(n) that prints the first n numbers of the
+# Fibonacci sequence (starting with 0, 1).
 # Example for n=6: 0 1 1 2 3 5
 # 
 # Hint: Use two variables to keep track of the last two numbers, update in a loop.
 # 
+def fibonacci(n):
+    a ,b=0,1
+    for i in range(n):
+        print(a,end = " ")
+        a ,b= b,a+b
+fibonacci(6)
