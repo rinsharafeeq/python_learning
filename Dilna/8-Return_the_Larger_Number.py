3c52200916210d12784b7923daacf5d3e3bddc46
Dilna <dilna@example.com>
2026-04-26 05:52:44 +0100

    return the large number

--- Dilna/8-Return_the_Larger_Number.py
+++ Dilna/8-Return_the_Larger_Number.py
@@ -1,1 +1,8 @@
-# Write a function max_of_two(a, b) that returns the larger number (use return instead of print). Then call it and print the returned value. Test with (8, 12) and (42, 42)
+# Write a function max_of_two(a, b) that returns the larger number
+# (use return instead of print).
+# Then call it and print the returned value. Test with (8, 12) and (42, 42)
+
+def max_of_two(a,b):
+    return "large number is",max(a,b)
+print(max_of_two(8,12))
+print(max_of_two(42,42))
