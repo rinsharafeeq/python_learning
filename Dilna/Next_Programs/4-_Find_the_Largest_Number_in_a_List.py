da344587ccfba273abdf955fbe116b40c7747555
Dilna <dilna@example.com>
2026-04-26 07:33:46 +0100

    find the largest number

--- Dilna/Next_Programs/4-_Find_the_Largest_Number_in_a_List.py
+++ Dilna/Next_Programs/4-_Find_the_Largest_Number_in_a_List.py
@@ -1,5 +1,13 @@
 # Write a function find_max(numbers) that returns the largest number in a list. Do not use max().
 # Test: [3, 7, 2, 9, 1] → 9.
 # 
-# Hint: Start by assuming the first element is the largest, then loop through the rest and update if you find a bigger one.
+# Hint: Start by assuming the first element is the largest,
+# then loop through the rest and update if you find a bigger one.
 # 
+def find_max(numbers):
+    large = numbers[0]
+    for i in numbers:
+        if i > large:
+            large = i
+    return large
+print(find_max([3,7,2,9,1]))
