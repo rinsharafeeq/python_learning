f9da16a6c819064cb892980b605142d5964c52a8
Dilna <dilna@example.com>
2026-04-26 06:53:25 +0100

    sum list

--- Dilna/Next_Programs/2-_Sum_of_a_List_of_Numbers.py
+++ Dilna/Next_Programs/2-_Sum_of_a_List_of_Numbers.py
@@ -1,5 +1,16 @@
-# Write a function sum_list(numbers) that takes a list of numbers and returns their sum. Do not use the built-in sum() function.
+# Write a function sum_list(numbers) that takes a list of numbers and returns their sum.
+# Do not use the built-in sum() function.
 # Test with: [1, 2, 3, 4, 5] → 15, [10, -2, 3] → 11.
 # 
 # Hint: Use a loop to accumulate the total.
-# 
+#
+from unittest import result
+
+
+def sum_list(numbers):
+    sum = 0
+    for i in numbers:
+        sum = sum +i
+    print("sum is ",sum)
+sum_list([1, 2, 3, 4, 5])
+sum_list([10, -2, 3])
