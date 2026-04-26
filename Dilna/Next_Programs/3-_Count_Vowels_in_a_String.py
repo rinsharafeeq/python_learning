7b686a5fe27cb9a93a593d008c53dda07246b372
Dilna <dilna@example.com>
2026-04-26 07:10:21 +0100

    count vowels in a string

--- Dilna/Next_Programs/3-_Count_Vowels_in_a_String.py
+++ Dilna/Next_Programs/3-_Count_Vowels_in_a_String.py
@@ -1,5 +1,17 @@
-# Write a function count_vowels(s) that returns the number of vowels (a, e, i, o, u, case‑insensitive) in the string s.
+# Write a function count_vowels(s) that returns the number of vowels
+# (a, e, i, o, u, case‑insensitive) in the string s.
 # Test: "Hello World" → 3 (e, o, o); "Python" → 1 (o).
 # 
-# Hint: Convert string to lowercase with s.lower(), then loop through each character and check if it's in "aeiou".
+# Hint: Convert string to lowercase with s.lower(),
+# then loop through each character and check if it's in "aeiou".
 # 
+def count_vowels(s):
+    i = s.lower()
+    count = 0
+    for c in i:
+      if c in "aeiou":
+        count += 1
+    return "no.of vowels",count
+print(count_vowels( "Hello World"))
+print(count_vowels('python'))
+
