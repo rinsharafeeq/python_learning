b4bb45722af1141b2d86e16bfc217c908618c167
Dilna <dilna@example.com>
2026-04-26 08:46:45 +0100

    guessing game

--- Dilna/Next_Programs/7-_Guessing_Game_using_while_and_input.py
+++ Dilna/Next_Programs/7-_Guessing_Game_using_while_and_input.py
@@ -8,4 +8,37 @@
 # 
 # Continues until the user guesses correctly, then prints "Correct!" and exits.
 # Bonus: Count how many guesses it took.
-# 
+#
+
+
+def guessing_game():
+    secret = 42
+    attempts = 0
+
+    while True:
+        # Get user input and convert to an integer
+        guess = int(input("Guess the secret number: "))
+        attempts += 1
+
+        if guess < secret:
+            print("Too low")
+        elif guess > secret:
+            print("Too high")
+        else:
+            print(f"Correct! It took you {attempts} guesses.")
+            break
+guessing_game()
+
+
+secret =42
+attempts = 0
+while True:
+    number = int(input("Guess the secret number: "))
+    attempts += 1
+    if number<secret:
+        print("Too low")
+    elif number>secret:
+        print("Too high")
+    elif number==secret:
+        print(f"Correct! It took you {attempts} guesses.")
+        break
