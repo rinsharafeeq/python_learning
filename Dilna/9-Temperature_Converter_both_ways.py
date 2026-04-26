463a0f6f4a45176950a833f560ad709c9294b06c
Dilna <dilna@example.com>
2026-04-26 06:01:43 +0100

    celsius to fahrenheit and fahrenheit to calsius

--- Dilna/9-Temperature_Converter_both_ways.py
+++ Dilna/9-Temperature_Converter_both_ways.py
@@ -1,1 +1,10 @@
-# Write two functions: to_celsius(f) and to_fahrenheit(c). Each should return the converted value. Then call them and print the results.
+# Write two functions: to_celsius(f) and to_fahrenheit(c).
+# Each should return the converted value. Then call them and print the results.
+def to_celsius(f):
+     celsius =( f-32)*5/9
+     print(celsius)
+def to_fahrenheit(c):
+    fahrenheit = (c*9/5)+32
+    print(fahrenheit)
+to_celsius(34)
+to_fahrenheit(28)
