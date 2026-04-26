afc4004da1d5db91f0b0b29fcc0a798458dcc867
Dilna <dilna@example.com>
2026-04-26 08:56:01 +0100

    unique elements

--- Dilna/Next_Programs/8-_Remove_Duplicates_from_a_List.py
+++ Dilna/Next_Programs/8-_Remove_Duplicates_from_a_List.py
@@ -1,5 +1,14 @@
-# Write a function unique_elements(lst) that returns a new list containing only the unique elements of lst (preserve order of first appearance).
+# Write a function unique_elements(lst) that returns a new list containing only
+# the unique elements of lst (preserve order of first appearance).
 # Example: [1, 2, 2, 3, 1, 4] → [1, 2, 3, 4]
 # 
 # Hint: Loop through the list and add to a new list only if it's not already there.
 # 
+def unique_elements(lst):
+    new_lst = []
+    for i in lst:
+        if i not in new_lst:
+            new_lst.append(i)
+
+    print(new_lst)
+unique_elements( [1, 2,2,2,3, 3, 4,4,2])
