d6da592d862aee0fe683a16aedbee97935e092dc
Dilna <dilna@example.com>
2026-04-26 06:14:06 +0100

    simple calculate

--- Dilna/10-Simple_Calculator.py
+++ Dilna/10-Simple_Calculator.py
@@ -1,4 +1,5 @@
-# Write a function calculate(a, b, op) where op is a string ("+", "-", "*", "/"). It should print the result of the operation. If op is unrecognized, print "Unknown operation".
+# Write a function calculate(a, b, op) where op is a string ("+", "-", "*", "/").
+# It should print the result of the operation. If op is unrecognized, print "Unknown operation".
 
 #Test with:
 
@@ -13,3 +14,19 @@
 #(8, 2, "%") → "Unknown operation"
 
 #========================
+def calculate(a, b, op):
+    if op == "+":
+        return a+b
+    elif op == "-":
+        return a-b
+    elif op == "*":
+        return a*b
+    elif op == "/":
+        return a/b
+    else:
+        return "unknown operation"
+print(calculate(10, 5, "+"))
+print(calculate(10, 5, "-"))
+print(calculate(10, 5, "*"))
+print(calculate(10, 5, "/"))
+print(calculate(8,  2, "%"))
