44b97e7753273befc34a572ed4644b39affefe4c
Dilna <dilna@example.com>
2026-04-26 05:39:50 +0100

    repeat string

--- Dilna/7-String_Repeater.py
+++ Dilna/7-String_Repeater.py
@@ -1,1 +1,8 @@
-# Write a function repeat_string(s, times) that prints the string s repeated times times (use the * operator). Test with ("Hi", 3), ("Ha", 5)
+# Write a function repeat_string(s, times) that prints the string s repeated times
+# (use the * operator). Test with ("Hi", 3), ("Ha", 5)
+def repeat_string (s, times):
+    print(s * times)
+repeat_string("Hi",3)
+repeat_string("Ha",5)
+
+
