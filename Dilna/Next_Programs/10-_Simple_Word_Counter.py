30e9e067d4f82a44fb91cea39ceec504dab3bddb
Dilna <dilna@example.com>
2026-04-26 09:23:29 +0100

    word counter

--- Dilna/Next_Programs/10-_Simple_Word_Counter.py
+++ Dilna/Next_Programs/10-_Simple_Word_Counter.py
@@ -1,5 +1,18 @@
-# Write a function word_count(text) that receives a string of multiple words (separated by spaces) and returns a dictionary (or just prints) how many times each word appears.
+# Write a function word_count(text) that receives a string of multiple words
+# (separated by spaces) and returns a dictionary (or just prints)
+# how many times each word appears.
 # Example: "hello world hello" → hello: 2, world: 1
 # 
 # Hint: Use text.split() to get a list of words, then a dictionary to count.
 # 
+def word_counter(text):
+    words = text.split()
+    dictionary = {}
+    for word in words:
+        if word in dictionary:
+            dictionary[word] = dictionary[word] + 1
+        if word not in dictionary:
+            dictionary[word] = 1
+
+    print(dictionary)
+word_counter( "hello world hello how are you you")
