51fcaf5c19c3230559edcf59d3b23813c68c4273
Dilna <dilna@example.com>
2026-05-01 15:50:31 +0530

    filtering even numbers

---
+++ Dilna/2-New-Programs/2-program.py
@@ -0,0 +1,11 @@
+
+#Task: Create a function filter_even(numbers) which returns a new list containing only the even numbers from the input list.
+#Filter even numbers from a list
+def filter_even(numbers):
+
+    list_even = []
+    for num in numbers:
+        if num % 2 == 0:
+            list_even.append(num)
+    return list_even
+print(filter_even([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]))
