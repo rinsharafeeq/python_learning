8e2429ba5c7b6622fa3cb9586fae9d5fb67cfcb4
Dilna <dilna@example.com>
2026-05-01 14:13:47 +0530

    avg

--- Dilna/4-Celsius_Fahrenheit.py
+++ Dilna/4-Celsius_Fahrenheit.py
@@ -2,7 +2,7 @@
 # the formula (celsius * 9/5) + 32 and prints the result. Test with 0, 100, -40.
 def c_to_f(celsius):
     print("farenheit")
-    return celsius *9/5+32
+    return celsius *9/5+32  #or # print(celsius*9/5+32)
 print(c_to_f(0))
 print(c_to_f(100))
 print(c_to_f(-40))
