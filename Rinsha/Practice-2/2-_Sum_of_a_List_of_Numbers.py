f760b8a322d0d79fe202ac567ca7581f28de609e
Rinsha Rafeeq <rinsharafeeq55@gmail.com>
2026-05-01 14:39:24 +0530

    sum of a list of numbers

--- Rinsha/Practice-2/2-_Sum_of_a_List_of_Numbers.py
+++ Rinsha/Practice-2/2-_Sum_of_a_List_of_Numbers.py
@@ -3,3 +3,12 @@
 # 
 # Hint: Use a loop to accumulate the total.
 # 
+def sum_list(numbers):
+    total = 0
+    for num in numbers:
+        total += num
+    return total
+
+# Testing the function
+print(sum_list([1, 2, 3, 4, 5]))
+print(sum_list([10, -2, 3]))
