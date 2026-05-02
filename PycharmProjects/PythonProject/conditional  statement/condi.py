9fd61df10f6f7857c6ef3f37118e8b58c7f86b55
Rinsha Rafeeq <rinsharafeeq55@gmail.com>
2026-05-02 13:18:45 +0530

    conditional statement 1

---
+++ PycharmProjects/PythonProject/conditional  statement/condi.py
@@ -0,0 +1,40 @@
+# a=-98
+# if a>0:
+#     print("the number is positive")
+# a=20
+# if  (a%2)==0:
+#     print("even")
+# a=int(input("enter a number :"))
+# if a>0:
+#     print("the number is positive")
+# else:
+#     print("the number is negative or zero")
+
+# if a%2==0:
+#     print("the number is even")
+# else:
+#     print("the number is odd")
+
+# a=6
+# if a%5==0:
+#     print("the number is multiple of 5")
+# else:
+#     print("the number is not multiple of 5")
+
+
+# n= int(input("enter  a number"))
+# if (n%5==0 and n%7==0):
+#     print(n,"is divisible by 5 and 7")
+# elif n%5==0:
+#     print(n,"is divisible by 5 only")
+# elif n%7==0:
+#     print(n," is divisible by 7 only")
+# else:
+#     print(n,"is not divisible by 5 and 7 ")
+#    print(n,"is negative")
+
+
+# elif n==0:
+#     print(n," is 0")
+# else:
+# print(n,"is negative")
