ce1031a2b22283f13685321ad359a593b17146c8
Rinsha Rafeeq <rinsharafeeq55@gmail.com>
2026-05-02 13:30:15 +0530

    set

---
+++ PycharmProjects/PythonProject/data collection/set.py
@@ -0,0 +1,22 @@
+# set={1,2,3,4.9,4,5,'s',4,4,5 }
+# print(type(set))
+# print(set)
+# print(len(set))
+# set.add(44)
+
+# set.update({45,65,})
+# print(set)
+# set.pop()
+# print(set)
+# set.remove(2)
+# print(set)
+# set1={1,2,3,4,5,6}
+# set2={4,5,6,7,8,9,0}
+# print(set1.union(set2))
+# print(set1.intersection(set2))
+# print(set1.difference(set2))
+#
+
+lst=[1,2,2,1,1,1,1,3,3,3,3,4,4,4,4,1]
+set1=set(lst)
+print(set1)
