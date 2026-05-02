4d976961f6de0af1052f971a5afd59c76deb19e1
Rinsha Rafeeq <rinsharafeeq55@gmail.com>
2026-05-02 13:37:41 +0530

    opretions

---
+++ PycharmProjects/PythonProject/operators/oper.py
@@ -0,0 +1,24 @@
+# a=10
+# b=4
+# print(a%b)
+# a=10
+# b=3
+# print(a//b)
+# a=3
+# b=4
+# print(a**b)
+# a=4
+# a+=7
+# print(a)
+# print(4<6 and 8>0)
+# print(4>8 and 2<6)
+# print(5<8 or 4>5)
+# print(5>9 )or 5<4)
+# print(not 4>7
+d=4
+f=454
+s=43
+w=4
+print(d is f)
+print(s is  not s)
+print(d is w)
