a03aa16e04ae9a3beaf5426920eb98c0f264d166
Rinsha Rafeeq <rinsharafeeq55@gmail.com>
2026-05-02 13:29:31 +0530

    list

---
+++ PycharmProjects/PythonProject/data collection/list.py
@@ -0,0 +1,48 @@
+# lst=input('enter the list elements :').split()
+# print(lst)
+# for i in lst:
+#     if len(i)%2==0:
+#       print(i,'is the even length ')
+
+ # lst=[1,2,3,4,[2,3,4,],3,54,7]
+ # print(lst[4][2])
+# lst=[1,2,3,[4,5,[6,7],8],9]
+# print(lst[3])
+# print(lst[3][2])
+# print(lst[3][2][0])
+
+ # lst=input('enter the list elements :').split()
+# print(lst)
+# sum=0
+# for i in lst:
+#     sum=sum+int(i)
+# print(sum)
+
+
+# lst=[1,1,1,1,1,2,2,2,2,2,3,3,3,3,3,3,4,4,4,4,4,4,5,5,5,5,5,5]
+# newlst=[]
+# for i in lst:
+#    if i not in  newlst :
+#        newlst.append(i)
+# print(newlst)
+# lst=[1,2,3,4,5,6]
+# sum=0
+# for i in lst:
+#     sum=sum+i
+# print(sum)
+
+# s=input('enter a string :')
+# dcount=0
+# acount=0
+# for i in s:
+#     if i.isdigit():
+#         dcount=dcount+1
+#     elif i.isalpha():
+#         acount=acount+1
+# print(dcount)
+# print(acount)
+# lst=[3,1,7,9,0,4]
+# print(lst)
+# lst.sort()
+# print(lst)
+# print(lst[-2])
