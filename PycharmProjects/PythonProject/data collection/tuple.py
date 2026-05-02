3c9d5b3e91ef88dcf12dddaab57e06c41e599ad3
Rinsha Rafeeq <rinsharafeeq55@gmail.com>
2026-05-02 13:30:43 +0530

    tuple

---
+++ PycharmProjects/PythonProject/data collection/tuple.py
@@ -0,0 +1,26 @@
+# tup=tuple(input('enter the elements :').split())
+# print(tup)
+# elst=[]
+# olst=[]
+# for i in tup:
+#     if int(i)%2==0:
+#         elst.append(i)
+#     else:
+#         olst.append(i)
+# print(tuple(elst),'even elements')
+# print(tuple(olst),'odd elements')
+
+# tup=(1,2,3,4,'apple')
+# print(type(tup))
+# print(tup[0:5])
+# tup=(1,2,3,4,5,4,5)
+# print(tup)
+# tup=tuple(input("enter the elements :").split())
+# print(tup)
+# print(type(tup))
+seet={1,2,3,4,5,7,7,7,8,9,7}
+print(type(seet))
+print(seet)
+print(len(seet))
+seet.pop()
+print(seet)
