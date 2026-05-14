67cfa3fe2a620da3558946437c05c03e2aa133a3
Rinsha Rafeeq <rinsharafeeq55@gmail.com>
2026-05-14 13:36:44 +0530

    date

--- Rinsha/3-Latest-programs/simple-programs/program-1.py
+++ Rinsha/3-Latest-programs/simple-programs/program-1.py
@@ -1,4 +1,10 @@
-Write a timestamp to a file
-What it does:
-Saves the current date and time into a file called timestamp.txt.
+#Write a timestamp to a file What it does: Saves the current date and time into a file called
+# timestamp.txt.
 
+import datetime
+x = datetime.datetime.now()
+result =x.strftime("%Y-%m-%d %H:%M:%S")
+
+with open("timestamp.txt","w") as f:
+  f.write(f"current date and time :{result}")
+print("Timestamp saved to timestamp.txt")
