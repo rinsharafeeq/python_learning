4042a51ec911ed738349cc4db98292918899c3f4
Rinsha Rafeeq <rinsharafeeq55@gmail.com>
2026-05-14 13:38:00 +0530

    data

--- Rinsha/3-Latest-programs/simple-programs/program-2.py
+++ Rinsha/3-Latest-programs/simple-programs/program-2.py
@@ -1,3 +1,8 @@
-Read a file and handle missing file error
-What it does:
-Tries to read data.txt. If it doesn't exist, prints a friendly message instead of crashing.
+# Read a file and handle missing file error
+# What it does:
+# Tries to read data.txt. If it doesn't exist, prints a friendly message instead of crashing.
+try:
+  with open("data.txt","r") as file:
+    print(file.read())
+except FileNotFoundError:
+    print("File 'data.txt' not found. Please create it and try again.")
